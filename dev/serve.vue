393761c8569697ca87108cde333fbac9a3bf7ccb
Joword <23089538@qq.com>
2023-03-28 16:34:19 +0800

    init project

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+import ExonCdsChart from '@/exon-cds-chart.vue';
+
+export default defineComponent({
+  name: 'ServeDev',
+  components: {
+    ExonCdsChart
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <exon-cds-chart />
+  </div>
+</template>
